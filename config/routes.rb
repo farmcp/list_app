d77135a1894811ff6636aa2792e02ffc7c4aa68d
Christopher Farm <farm.cp@gmail.com>
2012-06-01 17:34:59 -0700

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ListApp::Application.routes.draw do
+  get "static_pages/team"
+  root :to => 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
