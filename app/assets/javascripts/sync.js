5888a38867de0077705eab1b0afcc9c5e4356ce3
Christopher Farm <farm.cp@gmail.com>
2012-07-19 14:31:20 -0700

    adding help text.

--- app/assets/javascripts/sync.js
+++ app/assets/javascripts/sync.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   //need to figure out how to get the current_user.id in here and how to search the full_name
-  $("#search-box").tokenInput("/users/"+ $('#current_user_id').val() +"/following_followers.json", {propertyToSearch:"full_name", theme:"facebook"});
+  $("#search-box").tokenInput("/users/"+ $('#current_user_id').val() +
+      "/following_followers.json", {propertyToSearch:"full_name", theme:"facebook", hintText:"Enter a name in your network", searchingText:"Searching your network..."});
 });
