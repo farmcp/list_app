ac1c29e2906dd758d17de1df5e80c73004849747
Christopher Farm <farm.cp@gmail.com>
2012-07-26 12:04:56 -0700

    first food algo

--- app/helpers/sync_helper.rb
+++ app/helpers/sync_helper.rb
@@ -1,10 +1,21 @@
 module SyncHelper
   #create a recommendations engine here - right now just returns all of the users that were inputted and their restaurants
   def get_recommendations(users, location=nil)
+    #simple algorithm is to show the most popular places at the top and if there are ties then show the closest ones?
+    count_hash = Hash.new(0)
     list_items = users.collect(&:list_items).flatten
-    @recommendations = list_items.collect(&:restaurant).flatten
+    unsorted_rests = list_items.collect(&:restaurant).flatten
 
+    unsorted_rests.each do |rec|
+      count_hash[rec] += 1
+    end
+  
+    @restaurants = []
+    count_hash.sort_by{|k,v| @restaurants << k }.reverse
+  
+    puts @restaurants
+    puts count_hash
     #return a list of restaurants
-    return @recommendations
+    return @restaurants
   end
 end
