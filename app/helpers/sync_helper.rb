67cbf1fd33d9808d504cce161d845a37616824c9
hc5duke <hc5duke@gmail.com>
2012-07-26 17:49:14 -0700

    rewrite this part using enumerable

--- app/helpers/sync_helper.rb
+++ app/helpers/sync_helper.rb
@@ -2,18 +2,9 @@ module SyncHelper
   #create a recommendations engine here - right now just returns all of the users that were inputted and their restaurants
   def get_recommendations(users, location=nil)
     #simple algorithm is to show the most popular places at the top and if there are ties then show the closest ones?
-    count_hash = Hash.new(0)
-    list_items = users.collect(&:list_items).flatten
-    unsorted_rests = list_items.collect(&:restaurant).flatten
-
-    unsorted_rests.each do |rec|
-      count_hash[rec] += 1
-    end
-  
-    @restaurants = []
-    count_hash.sort_by{|k,v| @restaurants << k }.reverse
-
-    #return a list of restaurants
-    return @restaurants
+    restaurants = users.collect(&:list_items).flatten.collect(&:restaurant)
+    # histogram returns { restaurant => frequency }
+    popularity = restaurants.histogram
+    popularity.sort_by(&:last).collect(&:first)
   end
 end
