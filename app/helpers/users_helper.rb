4bd71698a06af2fe734bf6b0be5b596394000324
Christopher Farm <farm.cp@gmail.com>
2012-07-28 18:48:05 -0700

    add pictures to group syncs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
-  def gravatar_for(user, options = {size:50})
+  def gravatar_for(user, options = {size:''})
     gravatar_class = "gravatar"
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?r=r"
-    image_tag(gravatar_url, alt: user.first_name.to_s + user.last_name.to_s, class: gravatar_class)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?r=r&size=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.full_name.to_s, class: gravatar_class, :gravatar => options)
   end
 end
