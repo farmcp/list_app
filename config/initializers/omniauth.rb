fa1584823754e6df793b2a3359b2ec55480bc8f2
Christopher Farm <farm.cp@gmail.com>
2012-08-10 15:51:15 -0700

    trying to fix production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,7 @@ if Rails.env == 'development' || Rails.env =='test'
     provider :facebook, consumer_key, consumer_secret
   end
 else
+  OmniAuth.config.full_host = 'http://www.bitelist.com'
   Rails.application.config.middleware.use OmniAuth::Builder do
     consumer_key = '420852091284329'
     consumer_secret = '15568317829619690c4e7c8e39d25b9d'
