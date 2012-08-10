46e4e709e417f4f3dbbd5976516468a540fe0501
Christopher Farm <farm.cp@gmail.com>
2012-08-10 16:46:10 -0700

    environment vars?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,5 @@
-if Rails.env == 'development' || Rails.env =='test'
-  p Rails.env
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    consumer_key = '502583739757983'
-    consumer_secret = 'c6d07b598886de3eea30dda827ea2890'
-    provider :facebook, consumer_key, consumer_secret
-  end
-else
-  p Rails.env
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    consumer_key = '420852091284329'
-    consumer_secret = '15568317829619690c4e7c8e39d25b9d'
-    provider :facebook, consumer_key, consumer_secret
-  end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  consumer_key = ENV['FB_KEY']
+  consumer_secret = ENV['FB_SECRET']
+  provider :facebook, consumer_key, consumer_secret
 end
