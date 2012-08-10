bdd9a194a7e7f41d714594c45a27c54d0132ba05
Christopher Farm <farm.cp@gmail.com>
2012-08-10 08:00:37 -0700

    use facebook auth images and fix copy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,13 @@
 module UsersHelper
-  def gravatar_for(user, options = {size:''})
+  def gravatar_for(user, options = {size:'50'})
     gravatar_class = "gravatar"
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?r=r&size=#{options[:size]}"
-    image_tag(gravatar_url, alt: user.full_name.to_s, class: gravatar_class, :gravatar => options)
+    if user.image_url
+      image_tag(user.image_url, alt: user.full_name.to_s, class: gravatar_class, size: options[:size])
+    else
+      
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?r=r&size=#{options[:size]}"
+      image_tag(gravatar_url, alt: user.full_name.to_s, class: gravatar_class, :gravatar => options)
+    end
   end
 end
