02539efa6db6e06cfcde65a09fcf83ab3a2c2da4
hc5duke <hc5duke@gmail.com>
2012-08-12 17:13:01 -0700

    logic fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
     size = options.delete(:size) { 50 }
     image_tag(user.avatar_url, alt: user.full_name, class: 'gravatar', size: size)
   end
+
+  def active_unless_current_user
+    current_user?(@user) ? '' : 'active'
+  end
 end
