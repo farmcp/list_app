82698ef814d1f8c3efcf7d2a94cbbd88dec3c50b
hc5duke <hc5duke@gmail.com>
2012-08-12 23:44:50 -0700

    Wording

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    flash[:success] = "You're following #{@user.first_name} #{@user.last_name}!"
+    flash[:success] = "You're following #{@user.full_name}!"
     redirect_to @user
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    flash[:error] = "You don't follow #{@user.first_name} #{@user.last_name} anymore."
+    flash[:error] = "You've stopped following #{@user.full_name}."
     redirect_to @user
   end
 end
