c749e4305952f8b01da81103e496da362012dce1
hc5duke <hc5duke@gmail.com>
2012-08-12 22:08:35 -0700

    derp

--- lib/one_offs.rb
+++ lib/one_offs.rb
@@ -8,14 +8,10 @@ class OneOffs
   end
 
   def self.create_stories
-
     [ListItem, Comment, List].each do |clazz|
       clazz.all.each do |subject|
         subject.send :storify!
       end
     end
-    restaurants = ListItem.includes([:list, :restaurant, :user]).where(:lists => {:user_id => ids}).order('list_items.created_at')
-    comments = Comment.includes(:user).where(:user_id => ids).order('created_at')
-    lists = List.includes(:user).where(:user_id => ids).order('created_at')
   end
 end
