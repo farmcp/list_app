d8e241dc7acb18c1907c207ed812dbbddb8a9457
hc5duke <hc5duke@gmail.com>
2012-08-14 00:39:23 -0700

    Link avatar in most places

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,10 @@
 module UsersHelper
   def avatar_tag(user, options = {})
     size = options.delete(:size) { 50 }
-    image_tag(user.avatar_url, alt: user.full_name, class: 'gravatar', size: size)
+    link = options.delete(:link) { false }
+    link_to user do
+      image_tag(user.avatar_url, alt: user.full_name, class: 'gravatar', size: size)
+    end
   end
 
   def active_unless_current_user
