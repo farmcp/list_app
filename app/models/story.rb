d9b792af53a8f2de3800cfe25dc4878fef78f83f
Christopher Farm <farm.cp@gmail.com>
2012-08-20 23:47:53 -0700

    add more stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -10,7 +10,7 @@ class Story < ActiveRecord::Base
   belongs_to :relationship, :foreign_key => "subject_id"
   belongs_to :restaurant
 
-  self.per_page = 10
+  self.per_page = 20
 
   scope :includes_all, :include => {
     :user => [],
