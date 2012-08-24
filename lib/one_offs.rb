6636fd3a3d3f663657996d3bb5da88532438ac00
Christopher Farm <farm.cp@gmail.com>
2012-08-23 22:47:43 -0700

    add boston cities

--- lib/one_offs.rb
+++ lib/one_offs.rb
@@ -3,6 +3,7 @@ class OneOffs
   def self.add_sub_cities
     # SF
     sf = City.find_by_name('San Francisco')
+    bos = City.find_by_name('Boston')
     sf_cities = [
       'oakland',
       'pleasanton',
@@ -14,8 +15,29 @@ class OneOffs
       'fremont',
       'emeryville',
     ]
+
+    bos_cities = [
+      'cambridge',
+      'lowell',
+      'brockton',
+      'lynn',
+      'newton',
+      'somerville',
+      'lawrence',
+      'quincy',
+      'allston',
+      'peabody',
+      'nashua',
+      'framingham',
+      'waltham',
+      'worcester'
+    ]
     sf_cities.each do |sub_city|
       sf.sub_cities.create!(name: sub_city.downcase)
+    end
+
+    bos_cities.each do |sub_city|
+      bos.sub_cities.create!(name: sub_city.downcase)
     end
   end
 end
