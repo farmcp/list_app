0bc6250394417f51fb5f0b2a14cfc18721b2ce40
hc5duke <hc5duke@gmail.com>
2012-10-01 17:41:56 -0700

    use dev key by default

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  consumer_key = ENV['FB_KEY']
-  consumer_secret = ENV['FB_SECRET']
+  consumer_key = ENV['FB_KEY'] || '502583739757983'
+  consumer_secret = ENV['FB_SECRET'] || 'c6d07b598886de3eea30dda827ea2890'
   provider :facebook, consumer_key, consumer_secret
 end
