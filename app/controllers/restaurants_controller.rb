8b62e95581b2e028f4a45af21efa9f4ab90707cb
hc5duke <hc5duke@gmail.com>
2012-10-02 07:37:39 -0700

    got fb place returning identifier now

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -41,6 +41,7 @@ class RestaurantsController < ApplicationController
         :center => current_city.fb_center,
         :access_token => current_user.remember_token
       ).select{|place| Restaurant.acceptable_fb_place?(place)}
+      results.to_json(:only => [:identifier, :name])
     else
       json = '[]'
     end
--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -9,10 +9,23 @@ describe RestaurantsController do
   describe '#search' do
     before :each do
       @params = { :format => 'json' }
+
+      place = mock()
+      place.stub('identifier').and_return('1')
+      place.stub('name').and_return('some name')
+
+      FbGraph::Place.stub(:search).and_return([place])
     end
 
     context 'without query' do
       it 'should return nothing' do
+        get :search, @params
+        response.body.should == '[]'
+      end
+    end
+
+    context 'with query' do
+      it 'should return something' do
         get :search, @params
         response.body.should == '[]'
       end
