0e4eb63d32a86d2bc7c00f52bc42024224633a0c
Christopher Farm <farm.cp@gmail.com>
2012-10-20 12:56:03 -0700

    fix phone numbers of length 11)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,11 @@ module ApplicationHelper
 
   def link_to_phone(number_str)
     if number_str
-      formatted_str = number_str.sub(/(\d{3})(\d{3})(\d{4})/, "( \\1 ) \\2-\\3")
+      if number_str.length == 10
+        formatted_str = number_str.sub(/(\d{3})(\d{3})(\d{4})/, "( \\1 ) \\2-\\3")
+      elsif number_str.length == 11
+        formatted_str = number_str.sub(/(\d{1})(\d{3})(\d{3})(\d{4})/, "\\1 ( \\2 ) \\3-\\4")
+      end
       link_to formatted_str, "tel:#{number_str}", style: 'font-weight:normal; margin-top:3px;', class: 'btn btn-warning pull-left'
     end
   end
