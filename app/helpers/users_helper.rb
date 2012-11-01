73fcdbea43873df13435f3cddcdcfa2f3b1d6247
Christopher Farm <farm.cp@gmail.com>
2012-10-31 17:25:55 -0700

    fix copy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ module UsersHelper
     size = options.delete(:size) { 50 }
     link = options.delete(:link) { false }
     link_to user do
+      #data-title attribute is used for showing tooltip on restaurants/show
       image_tag(user.avatar_url, alt: user.full_name, class: 'gravatar', size: size, "data-title" => user.full_name)
     end
   end
