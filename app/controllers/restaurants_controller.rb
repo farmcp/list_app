73d0fe5464cba80e320223709a37b5b57db4448e
Christopher Farm <farm.cp@gmail.com>
2012-12-18 00:06:08 -0800

    dont require zip for fb search

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -51,8 +51,8 @@ class RestaurantsController < ApplicationController
         access_token: current_user.fb_search_token
       }
       results = FbGraph::Place.search(query, fb_search_options).map do |place|
-        if current_city.acceptable_fb_place?(place) && place.location.street != "" && place.location.zip != "" && place.location.city != ""
-          {id: place.identifier, name: place.name, address: place.location.street, zip: place.location.zip, city: place.location.city, url: place.fetch.picture}
+        if current_city.acceptable_fb_place?(place) && place.location.street != "" && place.location.city != ""
+          {id: place.identifier, name: place.name, address: place.location.street, city: place.location.city, url: place.fetch.picture}
         end
       end.compact
     else
