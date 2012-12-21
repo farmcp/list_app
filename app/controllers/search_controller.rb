e9763a39a3ab197b0dc094ef87f668da07816577
Christopher Farm <farm.cp@gmail.com>
2012-12-21 12:52:31 -0800

    set defaults for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,17 @@ class SearchController < ApplicationController
   before_filter :signed_in_user
 
   def search
-    users = User.text_search(params[:search])
-    restaurants = Restaurant.text_search(params[:search])
-    @results_user_count = users.count
-    @results_restaurant_count = restaurants.count
-    @results = users + restaurants
-
+    if params[:search] != ''
+      users = User.text_search(params[:search])
+      restaurants = Restaurant.text_search(params[:search])
+      @results_user_count = users.count
+      @results_restaurant_count = restaurants.count
+      @results = users + restaurants
+    elsif params[:search] = ''
+      @results = User.all
+      @results_user_count = User.all.count
+      @results_restaurant_count = 0
+    end
     # TODO get its own view
     render 'users/index'
   end
