6d3f6bbbc5ea2083b3f72d1e310e636050e90006
Christopher Farm <farm.cp@gmail.com>
2012-12-27 19:27:22 -1000

    go straight to users_path when no search element

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,18 +2,16 @@ class SearchController < ApplicationController
   before_filter :signed_in_user
 
   def search
+    #TO DO - make own view for search
     if params[:search] != ''
       users = User.text_search(params[:search])
       restaurants = Restaurant.text_search(params[:search])
       @results_user_count = users.count
       @results_restaurant_count = restaurants.count
       @results = users + restaurants
+      render 'users/index'
     elsif params[:search] = ''
-      @results = User.all
-      @results_user_count = User.all.count
-      @results_restaurant_count = 0
+      redirect_to users_path
     end
-    # TODO get its own view
-    render 'users/index'
   end
 end
