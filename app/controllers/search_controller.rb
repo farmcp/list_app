355b60af1a62009cc24b10e2c74b08a7fd077ab2
Christopher Farm <farm.cp@gmail.com>
2012-12-31 11:57:41 -1000

    fix json feed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,7 +15,7 @@ class SearchController < ApplicationController
       format.html { params[:search] != '' ? render('users/index') : redirect_to(users_path) }
       format.json { render :json => {
         :users => users.to_json(:only => [:id, :first_name, :last_name, :email]), 
-        :restaurants => restaurants.to_json(:except => :fb_place_id)
+        :restaurants => restaurants.to_json(:except => [:fb_place_id, :picture_url])
         } if users or restaurants
       }
     end
