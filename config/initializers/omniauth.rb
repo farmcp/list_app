20df35c4e9749fbee2e1435e88516c01ce9bbb45
Christopher Farm <farm.cp@gmail.com>
2013-02-03 19:21:24 -0800

    add publish_checkins

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   consumer_key = ENV['FB_KEY']
   consumer_secret = ENV['FB_SECRET']
-  provider :facebook, consumer_key, consumer_secret
+  provider :facebook, consumer_key, consumer_secret, {:scope => 'publish_checkins, publish_actions, publish_stream'}
 end
